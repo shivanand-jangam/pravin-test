682bb890582e60fdc0003fc9a250d53c83768b8a
shivanand-jangam <shivanand@infoglen.com>
2024-07-16 22:41:25 +0530

    Resloved compile issue

--- Pravin-IntegrationTests/src/main/java/com/test/ploymorphism/OverloadingExample.java
+++ Pravin-IntegrationTests/src/main/java/com/test/ploymorphism/OverloadingExample.java
@@ -9,11 +9,6 @@ public class OverloadingExample {
 		return a + b + c;
 	}
 	
-	public int calculateTotal(int a, int b, int c) {
-		System.out.println(a + b+c);
-		return a + b + c;
-	}
-
 	public int calculateTotal(int a, int b) {
 		System.out.println(a + b);
 		return a + b;
