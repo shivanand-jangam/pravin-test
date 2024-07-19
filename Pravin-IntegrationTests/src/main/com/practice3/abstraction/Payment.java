2d7bc790cec1d917119ccfe3d16e271282b1bc0f
kandepravin <kandepravin1@gmail.com>
2024-07-20 01:31:35 +0530

    polymorpism

---
+++ Pravin-IntegrationTests/src/main/com/practice3/abstraction/Payment.java
@@ -0,0 +1,24 @@
+package practice3.abstraction;
+
+
+
+abstract class Payment {
+    private double amount;
+
+    public Payment(double amt) {
+        amount = amt;
+    }
+
+    // Abstract methods
+    public abstract void makePayment();
+    public abstract void refundPayment();
+
+    // Concrete method
+    public void printReceipt() {
+        System.out.println("Receipt: Payment of " + amount + " processed.");
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
